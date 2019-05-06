5661eb20e878aa7e1353e1c4a56ac32968ed3747
ornokrean <or.nokrean@mail.huji.ac.il>
2019-05-06 18:55:05 +0300

    Started general workflow

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -3,72 +3,141 @@
 #include <cstdio>
 #include <atomic>
 #include <algorithm>
+#include <iostream>
+#include "Barrier.h"
 
-
-typedef struct JobContext {
+typedef struct JobContext
+{
     JobState state;
-    pthread_t* threads;
-    const InputVec& inputVec;
-    std::atomic<int>* atomic_index;
-    OutputVec& outputVec;
-    const int mTL;
-    const MapReduceClient& client;
-    std::vector<std::vector<std::pair<K2*,V2*>>> intermediaryVec;
+    pthread_t *threads;
+    const InputVec &inputVec;
+    std::atomic<int> *atomic_index;
+    OutputVec &outputVec;
+    int mTL;
+    const MapReduceClient &client;
+    std::vector<IntermediateVec> intermediaryVecs;
+    std::vector<IntermediateVec> reduceVecs;
 
-}JobContext;
+    Barrier *barrier;
+//
+//    JobContext(JobState state, pthread_t *threads, const InputVec &inputVec, std::atomic<int>
+//    *atomic_index, OutputVec &outputVec, const int mTL,
+//               const MapReduceClient &client,
+//               std::vector<IntermediateVec> intermediaryVecs)
+//    {
+//        this->state = state;
+//        this->threads = threads;
+//        this->inputVec = inputVec;
+//        this->atomic_index=atomic_index;
+//        this->mTL=mTL;
+//        this->outputVec=outputVec;
+//        this->client=client;
+//        this->intermediaryVecs=intermediaryVecs;
+//
+//    }
+} JobContext;
 
 
-struct ThreadContext {
+struct ThreadContext
+{
     int threadID;
     JobContext *context;
 };
 
+int compare(IntermediatePair first, IntermediatePair second)
+{
+    return first.first < second.first;
+}
 
-void* runThread(void* threadContext){
+void shuffle(void *context)
+{
+    auto jC = (JobContext *) context;
+    while()
+    {
+        K2 *max = jC->intermediaryVecs[0].back().first;
 
-    auto* tC= (ThreadContext*)threadContext;
+        for (int i = 0; i < jC->mTL; ++i)
+        {
+            if (jC->intermediaryVecs[i].back().first > max)
+            {
+                max = jC->intermediaryVecs[i].back().first;
+            }
+        }
+        // for each max
+        IntermediateVec vec;
+        for (int j = 0; j < jC->mTL; ++j)
+        {
+            if (!(*jC->intermediaryVecs[j].back().first < *max) && !(*max < *jC->intermediaryVecs[j]
+                    .back().first))
+            {
+                vec.push_back(jC->intermediaryVecs[j].back());
+                jC->intermediaryVecs[j].pop_back();
+            }
+        }
+        jC->reduceVecs.push_back(vec);
+    }
+}
+
+
+void *runThread(void *threadContext)
+{
+
+    auto *tC = (ThreadContext *) threadContext;
     InputVec inVec = tC->context->inputVec;
-    auto old = (size_t)tC->context->atomic_index->fetch_add(1);
+    auto tID = (size_t) tC->threadID;
+    auto old = (size_t) tC->context->atomic_index->fetch_add(1);
     //Map Phase:
-    while (old < inVec.size()){
-        std::pair<K1*,V1*> kv = inVec.at(old);
+    while (old < inVec.size())
+    {
+        std::cout << "Thread number " << tID << " accessing inputVec at: " << old << "\n";
+        InputPair kv = inVec.at(old);
         tC->context->client.map(kv.first, kv.second, threadContext);
-        old = (size_t)tC->context->atomic_index->fetch_add(1);
+        std::cout << kv.first << "   " << kv.second << "\n";
+        old = (size_t) tC->context->atomic_index->fetch_add(1);
     }
     //Sort Phase:
-    std::sort();
+    std::sort(tC->context->intermediaryVecs.at(tID).begin(),
+              tC->context->intermediaryVecs.at(tID).end(), compare);
+    //Barrier:
+    tC->context->barrier->barrier();
+    //Shuffle:
+    if (tC->threadID == 0)
+    {
+        shuffle(tC->context);
+    }
+    //Reduce:
 
 
-
+    return nullptr;
 }
 
-void executeJob(JobContext* context){
-
-    std::vector <void*> arr;
+void executeJob(JobContext *context)
+{
+    std::vector<void *> arr;
+    //TODO: Fix thread number issue
     for (int i = 0; i < context->mTL; ++i)
     {
-        arr.push_back(new ThreadContext{i,context});
+        arr.push_back(new ThreadContext{i, context});
         pthread_create(context->threads + i, nullptr, runThread, arr[i]);
-
     }
     //delete all
-
 }
-
 
 
 /*
  * The function produces a (K2*, V2*) pair
  * */
-void emit2 (K2* key, V2* value, void* context){
-    auto tC = (ThreadContext*) context;
-    tC->context->intermediaryVec.at((size_t)tC->threadID).push_back({key,value});
+void emit2(K2 *key, V2 *value, void *context)
+{
+    auto tC = (ThreadContext *) context;
+    tC->context->intermediaryVecs.at((size_t) tC->threadID).push_back({key, value});
 }
 
 /*
  * The function produces a (K3*, V3*) pair/
  * */
-void emit3 (K3* key, V3* value, void* context){
+void emit3(K3 *key, V3 *value, void *context)
+{
     //TODO: Gets called by the reduce function of client. Save the values into the output Vector.
 }
 
@@ -79,34 +148,44 @@ void emit3 (K3* key, V3* value, void* context){
  * outputVec- a vector of type std::vector<std::pair<K3*, V3*>>
  * multiThreadLevel- the number of worker threads to be used for running the algorithm
  * */
-JobHandle startMapReduceJob(const MapReduceClient& client,
-                            InputVec& inputVec, OutputVec& outputVec,
-                            int multiThreadLevel){
+JobHandle startMapReduceJob(const MapReduceClient &client,
+                            const InputVec &inputVec, OutputVec &outputVec,
+                            int multiThreadLevel)
+{
     //TODO: Create the struct with the context of the job. Start each thread with the map function.
     //Init the context:
     JobState state = {UNDEFINED_STAGE, 0.0};
     pthread_t threads[multiThreadLevel];
     auto *atomic_index = new std::atomic<int>(0);
-    JobContext context = {state, threads, inputVec, atomic_index, outputVec, multiThreadLevel, client};
+    std::vector<IntermediateVec> intermediaryVecs(multiThreadLevel);
+    std::vector<IntermediateVec> reduceVecs;
+    auto *barrier = new Barrier(multiThreadLevel);
+    auto *context = new JobContext{state, threads, inputVec, atomic_index, outputVec,
+                                   multiThreadLevel,
+                                   client, intermediaryVecs, reduceVecs, barrier};
 
 
-    executeJob(&context);
+    executeJob(context);
+    return context;
 }
 
 /*
  * The function gets the job handle returned by startMapReduceJob and waits until its finished
  * */
-void waitForJob(JobHandle job);
+void waitForJob(JobHandle job)
+{}
 
 /*
  * The function gets a job handle and checks for its current state in a given JobState struct
  * */
-void getJobState(JobHandle job, JobState* state);
+void getJobState(JobHandle job, JobState *state)
+{}
 
 /*
  * Releases all resources of a job. After calling, job will be invalid.
  * */
-void closeJobHandle(JobHandle job);
+void closeJobHandle(JobHandle job)
+{}
 
 
 
